a1228fc5c85a8abb1a16d6fcf1a2b192d4f6cc0a
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 20:05:52 -0600

    Create Unidad3Semana4Clase11Ejercicio2.cpp

---
+++ Unidad3Semana4Clase11Ejercicio2.cpp
@@ -0,0 +1,18 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal (){
+	int numero = 0 ;
+	int suma = 0 ;
+	
+	hacer {
+		suma += número;
+		
+	cout << " Ingrese un número: " ;
+	cin >> número;
+	}
+	while (número >= 0 );
+	cout << " \n La suma es " << sum << endl;
+	
+	devolver  0 ;
+}
