fefb355484f8ea5ef8556f5de74c12588154cf73
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:42:09 -0600

    Create Unidad2Semana4Clase12Ejercicio12.cpp

---
+++ Unidad2Semana4Clase12Ejercicio12.cpp
@@ -0,0 +1,13 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal (){
+arreglo int [ 100 ];
+para ( int i= 1 ; i<= 100 ;i++){
+	arreglo[i- 1 ] = i;
+}
+para ( int i= 99 ; i>= 0 ;i--){
+	cout << arreglo[i] << endl;
+}
+devolver  0 ;
+}
