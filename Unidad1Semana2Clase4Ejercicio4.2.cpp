479f1832d54867fd149942e4e6aa5824936a87fa
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:27:56 -0600

    Create Unidad1Semana2Clase4Ejercicio4.2.cpp

---
+++ Unidad1Semana2Clase4Ejercicio4.2.cpp
@@ -0,0 +1,10 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal (){
+	producto int = 3 ;
+	while ( producto <= 100 ){
+	producto = 3 * producto;
+	}
+	cout << " Producto es: " << producto;
+}
