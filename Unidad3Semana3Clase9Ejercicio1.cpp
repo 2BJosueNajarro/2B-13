0c785f7286ab63b0887f0c6368656badfc347f86
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:48:07 -0600

    Create Unidad3Semana3Clase9Ejercicio1.cpp

---
+++ Unidad3Semana3Clase9Ejercicio1.cpp
@@ -0,0 +1,15 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal ()
+{
+	numero int ;
+	hacer
+	{
+		cout <<   " Ingrese un numero " ;
+		cin >> número;
+	}
+	while (numero <= 100 );
+	sistema ( " PAUSA " );
+	devolver  0 ;
+}
