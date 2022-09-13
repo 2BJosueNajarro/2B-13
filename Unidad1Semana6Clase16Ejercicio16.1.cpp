40c2ba35959569c8573e11613b08e128050f660c
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:39:16 -0600

    Create Unidad1Semana6Clase16Ejercicio16.1.cpp

---
+++ Unidad1Semana6Clase16Ejercicio16.1.cpp
@@ -0,0 +1,18 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+ cubo int ( base int );
+principal ()
+    {
+    numero int ;
+    para (numero= 1 ; numero<= 5 ; numero++)
+    {
+    cout << " El cubo de " << numero << " es : " << cubo (numero) <<endl;
+	}
+	devolver  0 ;
+	}
+	int  cubo ( int base)
+    {
+    potencia int ;
+    potencia = base * base * base;
+    potencia de retorno ;
+	}
