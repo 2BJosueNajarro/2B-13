c6904dfc3153fbab1288714b45f58829316bb12d
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:44:10 -0600

    Create Unidad2Semana6Clase16Ejercicio16.cpp

---
+++ Unidad2Semana6Clase16Ejercicio16.cpp
@@ -0,0 +1,22 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+ numero int ( int a){
+	si (un < 0 ){
+		retorno - 1 ;
+	}
+	si  no (a > 0 ){
+		devolver  1 ;
+	}
+	más {
+		devolver  0 ;
+	}
+}
+int  principal (){
+	int a,b;
+	cout<< " \n Ingrese un numero:   \n  \n " ;
+	cin>>a;
+	cout<< " \n  \n  \n  \t " << num (a);
+	
+	devolver  0 ;
+}
