790fdfd4cdd884c3b73fb1e67bc128a7b1624bb5
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 20:05:11 -0600

    Create Unidad3Semana4Clase10Ejercicio1.cpp

---
+++ Unidad3Semana4Clase10Ejercicio1.cpp
@@ -0,0 +1,14 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal (){
+	int i = 1 ;
+	
+	hacer {
+		cout << i << "  " ;
+		++yo;
+	}
+	mientras (i <= 5 );
+	
+	devolver  0 ;
+}
