7a9e86df7e90cffe5b55365715a721c6ebc751f6
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:38:04 -0600

    Create Unidad1Semana5Clase15Ejercicio15.1.cpp

---
+++ Unidad1Semana5Clase15Ejercicio15.1.cpp
@@ -0,0 +1,12 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal (){
+	numero int = 0 ;
+	while (numero <= 100 ){
+		si (numero % 2 == 0 ){
+			cout << numero << endl;
+		}
+		numero = numero + 1 ;
+	}
+}
