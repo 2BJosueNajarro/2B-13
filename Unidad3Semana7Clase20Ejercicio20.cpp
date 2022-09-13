4f6e470e76a0bede6fa3e78097624855a4489c25
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 20:08:42 -0600

    Create  Unidad3Semana7Clase20Ejercicio20.cpp

---
+++ Unidad3Semana7Clase20Ejercicio20.cpp
@@ -0,0 +1,17 @@
+# incluir  < iostream >
+# include  < conio.h >
+utilizando el espacio de  nombres  estándar ;
+int  principal (){
+	intx ,n,c= 0 ,s= 0 ;
+	cout << " Ingresar cantidad de numeros: " ;
+	cin>>n;
+	mientras (c<n)
+	{
+		cout<< " Ingresar numero: " ;
+		cin>>x;
+		s=s+x;
+		c=c+ 1 ;
+	}
+	cout<< " La suma de los numeros es: " <<s;
+	obtener ();
+}
