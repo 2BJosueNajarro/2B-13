c2566f2a74634641d9369758dda32e662a2160b9
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:48:48 -0600

    Create Unidad3Semana3Clase9Ejercicio2.cpp

---
+++ Unidad3Semana3Clase9Ejercicio2.cpp
@@ -0,0 +1,19 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+int  principal ()
+{
+	int opcion = 0 ;
+	hacer
+	{
+		cout << " Ingrese 1 para mostrar mensaje \n " ;
+		cout << " Ingrese 2 para hacer algo mas \n " ;
+		cout << " Ingrese 0 para salir \n " ;
+		
+		cout << " Ingrese una opcion a ejecutar \n " ;
+		cin >> opción;
+	}
+	while (opcion != 0 );
+	sistema ( " PAUSA " );
+	devolver  0 ;
+}
