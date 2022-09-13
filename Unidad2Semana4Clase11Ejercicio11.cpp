f536c022349ebf01334c30ab4e3031e0dc34e393
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:41:24 -0600

    Create Unidad2Semana4Clase11Ejercicio11.cpp

---
+++ Unidad2Semana4Clase11Ejercicio11.cpp
@@ -0,0 +1,17 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+float  calcularPerimetro ( float );
+
+int  principal (){
+	lado doble ;
+	cout << " Ingrese el lado del cuadrado: " << endl;
+	cin >> lado;
+	cout << " El perimetro del cuadrado es: " << calcularPerimetro (lado) ;
+	devolver  0 ;
+}
+
+float  calcularPerimetro ( float lado){
+	doble perimetro = lado * 4 ;
+perimetro de retorno ;
+}
