0f0ad4dab898db517dc8c990f8a1c48b3cc32b4a
2BJosueNajarro <113318798+2BJosueNajarro@users.noreply.github.com>
2022-09-12 19:43:33 -0600

    Create Unidad2Semana5Clase14Ejercicio14.cpp

---
+++ Unidad2Semana5Clase14Ejercicio14.cpp
@@ -0,0 +1,15 @@
+# incluir  < iostream >
+utilizando el espacio de  nombres  estándar ;
+
+float  convertirKilometro ( float milla){
+	doble kilometro = milla * 1.60934 ;
+kilómetro de vuelta ;
+}
+
+int  principal (){
+doble milla;
+cout << " Ingrese la cantidad en millas: " << endl;
+cin>> milla;
+cout << " La cantidad en kilometros es: " << convertirKilometro (milla);
+devolver  0 ;
+}
